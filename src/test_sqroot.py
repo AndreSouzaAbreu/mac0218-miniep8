2dea878f191dc098b1d458d7d68ebe0891e8915b
Andre Souza Abreu <andresouzaabreu@usp.br>
2022-06-08 08:51:33 -0400

    add concrete test cases to be tested
    Previously, I had added a unit test function to test the sqroot  module.
    Now, I added the actual data to be tested, which is basically  an  array
    of random numbers whose square root will be calculated.

--- src/test_sqroot.py
+++ src/test_sqroot.py
@@ -1,14 +1,21 @@
 from math import fabs
+from random import randint
 
 def test_sqrt(tests, errortol):
     passed = 0
     n = len(tests)
     for i in range(n):
         x = tests[i]
-        result = sqroot(x)
+        result = sqroot(x, errortol)
         if fabs(x - result*result) > errortol:
             print(f'test {i+1} failed')
         else:
             print(f'test {i+1} successful')
             passed += 1
     print(f'\nPassed {passed}/{n}')
+
+# actual test cases
+n = 10
+tests = [randint(10, 1000) for _ in range(n)]
+errortol = 0.01
+test_sqrt(tests, errortol)
